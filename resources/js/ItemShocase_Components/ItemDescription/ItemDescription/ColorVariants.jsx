4a4ea6172ee673c9b7684a4072e7c62e9d285cba
Tahmid Zaman Raad <tahzam406@gmail.com>
2024-04-26 10:08:03 +0600

    temp

--- resources/js/ItemShocase_Components/ItemDescription/ItemDescription/ColorVariants.jsx
+++ resources/js/ItemShocase_Components/ItemDescription/ItemDescription/ColorVariants.jsx
@@ -1,49 +1,64 @@
 import React from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 
-const ColorVariants = ({ colorVariants, selectedIndex, onChangeColor }) => {
+const ColorVariants = ({
+    colorVariants,
+    selectedIndex,
+    onChangeColor,
+    colorIndex,
+}) => {
     const theme = useTheme();
 
-    return (
-        <Box className="colors" mt={5}>
-            <Typography
-                variant="itemdescTitle"
-                display="block"
-                color={theme.palette.text.grey[500]}
-                my={1}
-            >
-                Colour
-            </Typography>
-            {colorVariants.map((colorVariant, i) => (
-                <Box
-                    key={i}
-                    sx={{
-                        backgroundSize: "cover",
-                        border: `2px solid ${
-                            selectedIndex.color === i
-                                ? theme.palette.text.grey[800]
-                                : theme.palette.text.grey[400]
-                        }`,
-                        mr: 1,
-                        transition: "0.2s all ease",
-                    }}
-                    display="inline-block"
-                    onClick={() => onChangeColor(i)}
+    if (colorVariants[0] == "") colorVariants.shift();
+    if (colorVariants.length > 0)
+        return (
+            <Box className="colors" mt={5}>
+                <Typography
+                    variant="itemdescTitle"
+                    display="block"
+                    color={theme.palette.text.grey[500]}
+                    mb={1.5}
                 >
-                    <img
-                        src={
-                            colorVariant.imgURL
-                                ? colorVariant.imgURL
-                                : "./assets/blank.jpg"
-                        }
-                        height="40"
-                        width="40"
-                        style={{ margin: "2px" }}
-                    />
-                </Box>
-            ))}
-        </Box>
-    );
+                    Colour <span style={{ fontWeight: "200" }}>|</span>{" "}
+                    <Typography
+                        variant="itemdescSubtitle"
+                        color={theme.palette.text.grey[500]}
+                        textTransform="initial"
+                        fontWeight="300"
+                    >
+                        {colorVariants[colorIndex]}
+                    </Typography>
+                </Typography>
+                {colorVariants.map((colorVariant, i) => (
+                    <Box
+                        key={i}
+                        sx={{
+                            backgroundSize: "cover",
+                            border: `2px solid ${
+                                selectedIndex.color === i
+                                    ? theme.palette.text.grey[800]
+                                    : theme.palette.text.grey[400]
+                            }`,
+                            mr: 1,
+                            transition: "0.2s all ease",
+                        }}
+                        display="inline-block"
+                        onClick={() => onChangeColor(i)}
+                    >
+                        <img
+                            src={
+                                colorVariant.imgURL
+                                    ? colorVariant.imgURL
+                                    : "./assets/blank.jpg"
+                            }
+                            height="40"
+                            width="40"
+                            style={{ margin: "2px" }}
+                        />
+                    </Box>
+                ))}
+            </Box>
+        );
 };
 
 export default ColorVariants;
