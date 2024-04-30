29ccf4139c49d1d6815988b212f32ec78ba4871e
Samir Fazal <samir.fazal225@gmail.com>
2024-05-01 00:33:52 +0600

    fixed bug

--- resources/js/Layouts/DefaultPageLayout.jsx
+++ resources/js/Layouts/DefaultPageLayout.jsx
@@ -1,15 +1,24 @@
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/Theme/theme_desktop";
 import { Link } from "@inertiajs/react";
+import Navigation from "@/Global_Components/Navigationbar/Index";
 
-export default function Guest({ children, auth }) {
+export default function DefaultPage({ children, auth }) {
     return (
+        <ThemeProvider theme={theme}>
+            <Navigation
+                collections={[]}
+                auth={auth}
+                alternativeColor={true}
+            />
             <div className="min-h-screen flex flex-col sm:justify-center items-center items-center pt-10 sm:pt-0 bg-white">
                 <div>
                     <Link href="/">
                         {/* <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" /> */}
                         <img
-                            className="w-60 mt-10"
-                            src="../assets/Kodeline kids_Black Logo.svg"
-                            alt="logo"
+                            className="w-80 mt-10"
+                            src="../assets/ThanksForShoping.jpg"
+                            alt="ThanksForShoping"
                         />
                     </Link>
                 </div>
@@ -17,5 +26,6 @@ export default function Guest({ children, auth }) {
                     {children}
                 </div>
             </div>
+        </ThemeProvider>
     );
 }
--- resources/js/Pages/ThankYou.jsx
+++ resources/js/Pages/ThankYou.jsx
@@ -1,15 +1,15 @@
-import GuestLayout from '@/Layouts/GuestLayout';
+import DefaultPageLayout from '@/Layouts/DefaultPageLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Thanks({ auth, customer }) {
     return (
-        <GuestLayout
-            auth={auth.user}
+        <DefaultPageLayout
+            auth={auth}
         >
             <Head title="Dashboard" />
 
             <div className="p-6 text-center text-gray-500 light:text-gray-100">Thanks for shoping {customer.name}</div>
             <div className="p-6 text-center text-gray-500 light:text-gray-100">you email is {customer.email}</div>
-        </GuestLayout>
+        </DefaultPageLayout>
     );
 }
